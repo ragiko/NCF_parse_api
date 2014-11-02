b826c2c3f5999e1af298fa4f52735b5cc1da2fe5
root <root@www7338uo.sakura.ne.jp>
2014-11-03 06:21:33 +0900

    first init

---
+++ parse.php
@@ -0,0 +1,17 @@
+<?php
+require 'vendor/autoload.php';
+use Parse\ParseClient;
+use Parse\ParseUser;
+use Parse\ParseQuery;
+use Parse\ParseException;
+use Parse\ParseObject;
+use Parse\ParseACL;
+use Parse\ParseGeoPoint;
+
+ParseClient::initialize('LicvGZYQ3x9rDtfiDnaNy42GmIJdP0TuoVBJBFZi', 'QmaCbGyfU0chwYJ4n77cM1lv3pZeeVxNfa0FGrLE', 'pwLv0m1SioJBnuqlI3mvZ0Cv6jDRoC0BIRImMgGO');
+
+// ==
+$userObject = ParseUser::logIn("my name", "my pass");
+$gps = new ParseGeoPoint(37.708813, -122.526398);
+$userObject->set("location", $gps);
+$userObject->save();
