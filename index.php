cfaa888527816039c5403fdb63fdbeb4c3e2ac53
treasure2014 <treasure2014@treasure2014.(none)>
2014-11-04 19:13:38 +0900

    cephonodes/NCF2014#11 API作成完了

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 require 'vendor/autoload.php';
+include("./gracenote-rhythm/GracenoteRhythm.class.php");
 
 use Parse\ParseClient;
 use Parse\ParseUser;
@@ -117,10 +118,107 @@ $app->get('/', function () {
         }
     }
 
+    echo "<pre>";
     print_r($user_musics);
+    echo "</pre>";
     
     // 10秒かかかるww 
 });
 
-$app->run();
+/*
+ * gracenote
+ */
+$app->get('/gracenote', function () {
+    $clientID  = "3425280"; // Put your Client ID here.
+    $clientTag = "CC3C40AF6BD3CB6C78CE6D5468603199"; // Put your Client Tag here.
 
+    try {
+        /* You first need to register your client information in order to get a userID.
+        Best practice is for an application to call this only once, and then cache the userID in
+        persistent storage, then only use the userID for subsequent API calls. The class will cache
+        it for just this session on your behalf, but you should store it yourself. */
+        $api = new Gracenote\WebAPI\GracenoteRhythm($clientID, $clientTag); // If you have a userID, you can specify as third parameter to constructor.
+        $userID = $api->register();
+
+        $api->setCountry("jpn");
+        $api->setLanguage("jpn");
+    
+        $musics = $api->createStationFromMood(65322)["ALBUM"];
+    } catch (Exception $e) {
+        $res = json_encode(array("status" => "error [$e]", "data" => array()));
+        echo $res;
+        return;
+    }
+
+    foreach ($musics as $music) {
+        $artist = $music["ARTIST"][0]["VALUE"];
+        $music_title = $music["TITLE"][0]["VALUE"];
+        $youtube_id = getVideoId($artist, $music_title);
+
+        if ($youtube_id !== "error!") {
+            break;
+        }
+    }
+
+    if (isset($youtube_id)) {
+        $res = json_encode(array(
+            "status" => "success", 
+            "data" => 
+            array(
+                "artist" => $artist,
+                "title" => $music_title,
+                "youtube_id" => $youtube_id
+            )
+        ));
+        echo $res;
+        return;
+    }
+    else {
+        $res = json_encode(array("status" => "error [No youtube_id]", "data" => array()));
+        echo $res;
+        return;
+    }
+
+    // TODO 文字数多いやつのけた方がいいかも
+    // TODO youtubeで見つかったやつにランダム
+});
+
+/*
+ * youtube
+ */
+function getVideoId($artist, $musicTitle){
+    // This code will execute if the user entered a search query in the form
+    // and submitted the form. Otherwise, the page displays the form above.
+    if ($artist != '' && $musicTitle != '') {
+        // Call set_include_path() as needed to point to your client library.
+        $serchQuery = $artist." ".$musicTitle;
+        $maxResults = 1;
+        /*
+         * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
+         * Google Developers Console <https://cloud.google.com/console>
+         * Please ensure that you have enabled the YouTube Data API for your project.
+         */
+        $DEVELOPER_KEY = 'AIzaSyDOWXXqjSREftoZ78WxCJNHWvVyQcL2ogc';
+        $client = new Google_Client();
+        $client->setDeveloperKey($DEVELOPER_KEY);
+        // Define an object that will be used to make all API requests.
+        $youtube = new Google_Service_YouTube($client);
+        // Call the search.list method to retrieve results matching the specified
+        // query term.
+        $searchResponse = $youtube->search->listSearch('id,snippet', array(
+            'q' => $serchQuery,
+            'maxResults' => $maxResults,
+        ));
+        // Add each result to the appropriate list, and then display the lists of
+        // matching videos, channels, and playlists.
+        foreach ($searchResponse['items'] as $searchResult) {
+            switch ($searchResult['id']['kind']) {
+            case 'youtube#video':
+                return $searchResult['id']['videoId'];
+            }
+        }
+    }
+    return "error!";
+}
+
+$app->run();
